93f29d98a5642da9c7745a777a230328dbdd9141
ErReich <nikolya.severus@mail.ru>
2021-01-26 14:10:28 +0300

    26.01.2021 Error correction

--- src/main/java/web/crud/dao/UserDAOImpl.java
+++ src/main/java/web/crud/dao/UserDAOImpl.java
@@ -6,39 +6,45 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import web.crud.entity.User;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Query;
+import javax.persistence.*;
 import java.util.List;
 
 @Repository
 public class UserDAOImpl implements UserDAO {
 
-    @Autowired
-    EntityManagerFactory entityManagerFactory;
-//    @PersistenceContext
-//    EntityManager em;
+//    @Autowired
+//    EntityManagerFactory entityManagerFactory;
+    @PersistenceContext
+    EntityManager em;
 
     @Override
     public List<User> getAllUsers() {
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
+//        EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-        return entityManager.createQuery("from User").getResultList();
+//        return entityManager.createQuery("from User").getResultList();
+        return em.createQuery("from User", User.class).getResultList();
     }
 
     @Override
     public void saveUser(User user) {
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        entityManager.unwrap(Session.class).save(user);
+//        EntityManager entityManager = entityManagerFactory.createEntityManager();
+//        entityManager.unwrap(Session.class).save(user);
+        em.persist(user);
     }
 
     @Override
     public void updateUser(User user) {
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
+//        EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-        Session session = entityManager.unwrap(Session.class);
-        Transaction transaction = session.beginTransaction();
-        Query query = session.createQuery("UPDATE User set name = :userName, " +
+//        Session session = entityManager.unwrap(Session.class);
+//        Transaction transaction = session.beginTransaction();
+//        Query query = session.createQuery("UPDATE User set name = :userName, " +
+//                "surname = :userSurname, age = :userAge where id = :userId");
+
+//        TypedQuery<User> query = em.createQuery("UPDATE User set name = :userName, " +
+//                "surname = :userSurname, age = :userAge where id = :userId", User.class);
+
+        Query query = em.createQuery("UPDATE User set name = :userName, " +
                 "surname = :userSurname, age = :userAge where id = :userId");
 
         query.setParameter("userName", user.getName());
@@ -46,9 +52,9 @@ public class UserDAOImpl implements UserDAO {
         query.setParameter("userAge", user.getAge());
         query.setParameter("userId", user.getId());
         query.executeUpdate();
-
-        transaction.commit();
-        session.close();
+//
+//        transaction.commit();
+//        session.close();
     }
 
 //    @Override
@@ -60,23 +66,26 @@ public class UserDAOImpl implements UserDAO {
 
     @Override
     public void deleteUser(User user) {
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        Session session = entityManager.unwrap(Session.class);
+//        EntityManager entityManager = entityManagerFactory.createEntityManager();
+//        Session session = entityManager.unwrap(Session.class);
+//
+//        Transaction transaction = session.beginTransaction();
 
-        Transaction transaction = session.beginTransaction();
+//        entityManager.remove(entityManager.contains(user) ? user : entityManager.merge(user));
+//
+//        transaction.commit();
+//        session.close();
 
-        entityManager.remove(entityManager.contains(user) ? user : entityManager.merge(user));
-
-        transaction.commit();
-        session.close();
+        em.remove(em.contains(user) ? user : em.merge(user));
     }
 
     @Override
     public User getUser(Integer id) {
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-
-        Session session = entityManager.unwrap(Session.class);
-
-        return session.load(User.class, id);
+//        EntityManager entityManager = entityManagerFactory.createEntityManager();
+//
+//        Session session = entityManager.unwrap(Session.class);
+//
+//        return session.load(User.class, id);
+        return em.find(User.class, id);
     }
 }
